5db54d3edc87c3f58b05e0592720ceb0a6c67021
MBP27 <MBP27@mac.home>
2025-08-13 11:04:28 +0200

    add selection cases

--- rive.js
+++ rive.js
@@ -28,7 +28,26 @@ const eventFire = (riveEvent) => {
 	const eventData = riveEvent.data;
 	const eventName = eventData.name;
 	const eventProperties = eventData.properties;
-	switch (eventName) {
+	const cardNumber = eventName.slice(-1);
+	switch (cardNumber) {
+		case "1":
+			console.log("1");
+			break;
+		case "2":
+			console.log("2");
+			break;
+		case "3":
+			console.log("3");
+			break;
+		case "4":
+			console.log("4");
+			break;
+		case "5":
+			console.log("5");
+			break;
+		case "6":
+			console.log("6");
+			break;
 		default:
 			break;
 	}
